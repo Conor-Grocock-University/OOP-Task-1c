4d6e8ff50439618078342d0133e3a64f891c0699
Chris <c.brewster@hotmail.co.uk>
2020-03-30 17:18:56 +0100

    Takes string input and splits into 3 date variables

--- OOP - Task 1C/Date.cpp
+++ OOP - Task 1C/Date.cpp
@@ -2,29 +2,37 @@
 #include <string>
 #include <iostream>
 
-Date::Date(const std::string& date) : date(date)
+Date::Date(std::string date)
 {
+	std::string delimiter = "/"; // creating a delimiter that will be used to split the string
+
+	size_t pos = 0;
+	std::string dates[3] = {}; // creating a fixed 3 item array of strings which will be the day / month / year
+	int i = 0; // incrementor
+
+	while ((pos = date.find(delimiter)) != std::string::npos) { // while there are "/" characters in the string
+		dates[i] = date.substr(0, pos); // storing the first, second & third split strings in the array
+		date.erase(0, pos + delimiter.length()); // removing the first position of the string (DD/MM/YYYY becomes MM/YYYY etc.)
+
+		i++;
+	}
+
+	// storing correct dates in variables
+	day = std::stoi(dates[0]);
+	month = std::stoi(dates[1]);
+	year = std::stoi(dates[2]);
 }
+
+// destructor
+Date::~Date()
+{
+} 
 
 const Date Date::CurrentDate()
 {
 	return Date; // returning current timestamp
 }
 
-int Date::GetDay() const
-{
-	return this->day;
-}
-
-int Date::GetMonth() const
-{
-	return this->month;
-}
-
-int Date::GetYear() const
-{
-	return this->year;
-}
 
 const std::string Date::ToFormattedString() const
 {
--- OOP - Task 1C/Date.h
+++ OOP - Task 1C/Date.h
@@ -3,21 +3,16 @@
 class Date {
 
 public:
-	Date(const std::string&);
 
-
+	Date(std::string);
+	~Date();
 	static const Date CurrentDate(); // static function to get current date
 	const std::string ToFormattedString() const;
-	int GetDay() const;
-	int GetMonth() const;
-	int GetYear() const;
-	//time_t GetTimstamp();
-
+	
 private:
-	std::string date;
+
 	int day;
 	int month;
 	int year;
-	//time_t timestamp;
 };
 
