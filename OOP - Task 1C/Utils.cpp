017478633ab316814b6c9d6637aa67444c4a8199
Chris <c.brewster@hotmail.co.uk>
2020-04-02 12:20:09 +0100

    Adds startWith() function to compare search text with game name

--- OOP - Task 1C/Utils.cpp
+++ OOP - Task 1C/Utils.cpp
@@ -11,3 +11,16 @@ char Utils::getCharFromUser()
 {
     return toupper(getLineFromUser()[0]);
 }
+
+bool Utils::startsWith(std::string& searchText, std::string& gameName)
+{
+	if (gameName.find(searchText) == 0) // if the search text is found at the beginning of game name (if the game name starts with search text)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
--- OOP - Task 1C/Utils.h
+++ OOP - Task 1C/Utils.h
@@ -8,6 +8,6 @@ class Utils
 public:
     static std::string getLineFromUser();
     static char getCharFromUser();
-
+    static bool startsWith(std::string&, std::string&);
     // TODO: 2 versions of ToUpper
 };
