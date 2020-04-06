49134f742e6340c2a272313fef4615d578aec67a
Conor Grocock <conor@grocock.email>
2020-04-06 06:34:26 +0100

    Added getters to Library item

--- OOP - Task 1C/LibraryItem.cpp
+++ OOP - Task 1C/LibraryItem.cpp
@@ -1,10 +1,20 @@
 #include "LibraryItem.h"
 
 LibraryItem::LibraryItem(const Date& date,const Game* game)
-	: purchased(date), game(game)
+	: game(game), purchased(date)
 {
 }
 
 LibraryItem::~LibraryItem()
 {
+}
+
+const Game* LibraryItem::GetGame() const
+{
+	return game;
+}
+
+const Date& LibraryItem::GetPurchaseDate() const
+{
+	return purchased;
 }
--- OOP - Task 1C/LibraryItem.h
+++ OOP - Task 1C/LibraryItem.h
@@ -8,7 +8,10 @@ class LibraryItem
 	public:
 		LibraryItem(const Date&,const Game*);
 		~LibraryItem();
+
+		const Game* GetGame() const;
+		const Date& GetPurchaseDate() const;
 	private:
 		const Game* game;
-		const Date purchased; 
+		const Date& purchased; 
 };
