95d496597530e1e191450868d79d00485c592114
Conor Grocock <conor@grocock.email>
2020-04-06 16:23:36 +0100

    Removed unused imports from source file

--- OOP - Task 1C/Source.cpp
+++ OOP - Task 1C/Source.cpp
@@ -1,21 +1,10 @@
-#include <iostream>
-#include <string>
 #include "Application.h"
-#include "Account.h"
-#include "User.h"
-#include "Player.h"
-#include "Admin.h"
-#include "DataParser.h"
-#include "Utils.h"
 #include "MainMenu.h"
-#include "List.h"
 
 void main()
 {
     Application app;
     app.load();
-
     Menu::MainMenu menuSystem("Main", &app);
-
     app.save();
 }
